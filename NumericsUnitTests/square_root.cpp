2e374a582c027c952ccec1902d25e21a5a556bba
Alexandre PARENT <1237801@cstj.qc.ca>
2019-03-01 11:49:18 -0500

    fixup! Save and share

---
+++ NumericsUnitTests/square_root.cpp
@@ -0,0 +1,22 @@
+#include <unordered_map>
+#include <CppUnitTest.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace tests {
+  namespace numerics {
+    TEST_CLASS(SquareRoot){
+    private:
+      std::unordered_map<double, double> const data{
+	  
+	  };
+
+      public:
+
+        TEST_METHOD(TestMethod1){
+            // TODO: Ici, votre code de test
+        }
+
+    };
+  }
+}
